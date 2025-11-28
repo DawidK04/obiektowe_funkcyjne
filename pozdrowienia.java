75a52c0a5643059d7092183390cfe63abc5a401c
DawidK04 <karolakdawid04@gmail.com>
2025-11-28 16:11:23 +0100

    Add pozdrowienia.java with greeting logic

---
+++ pozdrowienia.java
@@ -0,0 +1,11 @@
+public class pozdrowienia {
+    public static void main(String[] args){
+        String c = "Hello";
+        if(c==null)
+            System.out.println("pusty");
+        else
+            System.out.println("niepusty");
+        System.out.println(c.equals("Hello"));
+            "Hello".equals(c);
+    }
+}

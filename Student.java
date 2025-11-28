1f86052ebf76e50c8cba14630a0a36881cf88368
DawidK04 <karolakdawid04@gmail.com>
2025-11-28 17:34:48 +0100

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,10 @@
+package dziedziczenie;
+
+public class Student extends Pracownik{
+    private String spec
+            public Student(int n, String s) {
+        super(n);
+        spec = s;
+            }
+            
+}

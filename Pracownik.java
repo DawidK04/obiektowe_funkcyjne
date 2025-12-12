25bdead6bdd80e8d0da426a2b6a0f5841d817a71
DawidK04 <karolakdawid04@gmail.com>
2025-12-12 17:30:30 +0100

    Update Pracownik.java
    Refactor Pracownik class to extend Osoba and update methods.

--- Pracownik.java
+++ Pracownik.java
@@ -1,28 +1,31 @@
-package dziedziczenie;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-public class Pracownik {
+public class Pracownik extends Osoba{
     // pole(a)
-    private String nazwa;
+    // private String nazwa;
     private double pensja;
     private Date dzat; // data zatrudnienia
 
     // konstruktor
     public Pracownik(String n, double p, int r, int m, int d) {
-        nazwa = n;
+        // nazwa = n;
+        super (n);
         pensja = p;
         GregorianCalendar rmd = new GregorianCalendar(r, m - 1, d);
         dzat = rmd.getTime();
     }
 
-    public String getNazwa() {
-        return nazwa;
-    }
+    // public String getNazwa() {return nazwa;}
     public double getPensja() {
         return pensja;
     }
     public Date getDzat() {
         return dzat;
     }
+
+    public void premia(double procent){double p = pensja * procent / 100;
+    pensja += p;}
+
+    public String getInfo() {return String.format("Wypłata: %.2f zł", pensja);}
 }

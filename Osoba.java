11ae3f312c46bca72a38cf358cd79d7e08853d4e
DawidK04 <karolakdawid04@gmail.com>
2025-12-12 17:32:45 +0100

    Create abstract class Osoba with basic methods

---
+++ Osoba.java
@@ -0,0 +1,6 @@
+public abstract class Osoba {
+    public abstract String getInfo();
+    private String nazwa;
+    public Osoba(String n) {nazwa = n;}
+    public String getNazwa() {return nazwa;}
+}

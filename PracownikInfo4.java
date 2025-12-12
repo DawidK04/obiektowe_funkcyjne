eb7c1d56d020db35bc2a577dbf77244997153745
DawidK04 <karolakdawid04@gmail.com>
2025-12-12 17:31:26 +0100

    Update PracownikInfo4.java

--- PracownikInfo4.java
+++ PracownikInfo4.java
@@ -1,18 +1,13 @@
-package dziedziczenie;
 import java.util.*;
 public class PracownikInfo4 {
     public static void main(String[] args) {
-        Kierownik k = new Kierownik("AZ", 3000.00, 2024, 11, 14);
 
-        Pracownik[] wol = new Pracownik[4];
+        Osoba[] wol = new Osoba[3];
         wol[0] = new Pracownik("JK", 3000.00, 2025, 11, 14);
         wol[1] = new Pracownik("JK", 3000.00, 2025, 11, 14);
-        wol[2] = new Pracownik("JK", 3000.00, 2025, 11, 14);
-        wol[3] = k;
-        for (Pracownik p : wol) {
-            System.out.println("Nazwa: " + p.getNazwa() + " Pensja: " + p.getPensja() + " Wyrok: " + p.getDzat());
+        wol[2] = new Student("XY", "Biochem");
+        for (Osoba p : wol) {
+            System.out.println("Nazwa: " + p.getNazwa() + ", " + p.getInfo());
         }
-
-
     }
 }
